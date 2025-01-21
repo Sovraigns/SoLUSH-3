c2343edc6bed5a60061ba318a8f91bdcdc1fb658
zjkree <email@email.com>
2025-01-21 08:59:21 +0100

    Add more local mutation

--- offchain/src/gp/generate_spec.rs
+++ offchain/src/gp/generate_spec.rs
@@ -47,7 +47,7 @@ impl InstructionSet {
             InstructionAtom::Opcode(op) => UntypedAst::Instruction(op.clone()),
             InstructionAtom::EphemeralInt => {
                 // For ephemeral int, produce a random literal in some range
-                let val = rng.gen_range(-100..100);
+                let val = rng.gen_range(-30..30);
                 UntypedAst::IntLiteral(val)
             }
         }
--- offchain/src/gp/mod.rs
+++ offchain/src/gp/mod.rs
@@ -3,3 +3,4 @@ pub mod genetic_ops;
 pub mod generate;
 pub mod generate_spec;
 pub mod mutation;
+pub mod local_mutation;
